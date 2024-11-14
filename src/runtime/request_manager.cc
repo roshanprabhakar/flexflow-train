b885c63f216a55047fa6486a86e7592112256b60
fruitea <aetiurf@gmail.com>
2024-11-14 07:53:40 -0800

    fix: compile bug

--- src/runtime/request_manager.cc
+++ src/runtime/request_manager.cc
@@ -2475,7 +2475,7 @@ void RequestManager::serve_spec_infer_sync(FFModel *llm) {
     assert(im->model_weights_loaders.find(llm) !=
            im->model_weights_loaders.end());
     // Load model weights
-    im->model_weights_loaders[llm]->load_weights(llm);
+    im->model_weights_loaders[llm]->load_weights_parallel(llm, ctx, runtime);
     // init operators
     im->init_operators_inference(llm);
   }
@@ -2486,7 +2486,7 @@ void RequestManager::serve_spec_infer_sync(FFModel *llm) {
     assert(im->model_weights_loaders.find(ssm) !=
            im->model_weights_loaders.end());
     // Load model weights
-    im->model_weights_loaders[ssm]->load_weights(ssm);
+    im->model_weights_loaders[ssm]->load_weights_parallel(ssm, ctx, runtime);
     // init operators
     im->init_operators_inference(ssm);
   }
